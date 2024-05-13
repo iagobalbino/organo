2e58ddf2cddbd79f5ac5e2635165a1c10034ab32
Iago Balbino <iago_nobre@hotmail.com>
2024-05-13 16:46:09 -0300

    Caminho do banner atualizado em Banner.js e criado gif para README.md

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,9 +1,10 @@
 import './Banner.css';
+// import banner from '/public/imagens/banner.png';
 
 function Banner() {
   return (
     <header className='banner'>
-      <img src="/imagens/banner.png" alt="Banner principal do site" />
+      <img src='imagens/banner.png' alt="Banner principal do site" />
     </header>
   )
 }
